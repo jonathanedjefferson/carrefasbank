4205a6aa61c9d89e147aa4bded0597edbb23ac1a
Jonathan Silva <jonathan.silva@everymind.uol>
2026-01-15 10:27:35 -0300

    adicionei console log no botão Simulação de crédito

--- force-app/main/default/lwc/bcsf_cmp_CreditoPessoalAdesao/bcsf_cmp_CreditoPessoalAdesao.js
+++ force-app/main/default/lwc/bcsf_cmp_CreditoPessoalAdesao/bcsf_cmp_CreditoPessoalAdesao.js
@@ -132,6 +132,15 @@ export default class BCSF_cmp_CreditoPessoalAdesao extends LightningElement {
         return this.clienteValidado && this.subAreaPrincipal !== null && this.subAreaPrincipal.includes('Crédito Pessoal');
     }
 
+    connectedCallback() {
+    console.log('=== Debug showBotaoSimularCreditoPessoal ===');
+    console.log('clienteValidado:', this.clienteValidado);
+    console.log('subAreaPrincipal:', this.subAreaPrincipal);
+    console.log('subAreaPrincipal type:', typeof this.subAreaPrincipal);
+    console.log('Resultado final:', this.showBotaoSimularCreditoPessoal);
+    }
+
+
     get valorPreAprovado() {
         return formatarValor(this.status.valorPreAprovado);
     }
